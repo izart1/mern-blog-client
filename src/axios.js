aa9fec3949a2179336654f74b43c10a59a72160c
Artem Izmailov <77829898+izart1@users.noreply.github.com>
2023-01-21 01:22:54 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://localhost:5555',
-  baseURL: '62.113.96.241',
+  baseURL: '62.113.96.241:6804',
 });
 
 instance.interceptors.request.use(config => {
